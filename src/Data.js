053ade8cefb33b4e6a84797ee3f611da262eb9ca
kev1nnnnnnn <kevinbfv@gmail.com>
2022-09-16 16:23:52 -0400

    bar, line, pier

---
+++ src/Data.js
@@ -0,0 +1,32 @@
+export const UserData = [
+  {
+    id: 1,
+    name: 'Amazonino',
+    votos: 100,
+    userLost: 823,
+  },
+  {
+    id: 2,
+    name: 'Wilson Lima',
+    votos: 200,
+    userLost: 345,
+  },
+  {
+    id: 3,
+    name: 'Eduardo Braga',
+    votos: 300,
+    userLost: 555,
+  },
+  {
+    id: 4,
+    name: 'Ricardo Nicolau',
+    votos: 400,
+    userLost: 4555,
+  },
+  {
+    id: 5,
+    name: 'Henrique Oliveira',
+    votos: 500,
+    userLost: 234,
+  },
+];
